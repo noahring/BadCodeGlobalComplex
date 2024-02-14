724b16dfdac1cdd7b9d97a6c8d717b6e7a387461
Noah Ring <noahring@Noahs-MacBook-Air.local>
2024-02-13 19:48:56 -0500

    Finished

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -1,2 +1,2 @@
-public abstract class A {String n;double x;double y;public A(String n, double x, double y) {super();this.n = n;this.x = x;this.y = y;}public double m() {return x;}public void s(double x) {this.x = x;}public abstract double c();}
+public abstract class A {String n;double x;double y;public A(String n,double x,double y) {super();this.n = n;this.x = x;this.y = y;}public double m() {return x;}public void s(double x) {this.x = x;}public abstract double c();}
 
